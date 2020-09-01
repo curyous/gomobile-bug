d4d1d9e4fb2552d6b5a3e6414ac29a9e72d239fb
Brendon Duncan <brendon@coinline.co.nz>
2020-09-01 20:24:33 +1200

    Initial commit.

---
+++ adder.go
@@ -0,0 +1,6 @@
+package adder
+
+// Add3 adds 3
+func Add3(a int) int {
+	return a + 3
+}
---
+++ ios/GoMobile Bug/GoMobile Bug/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GoMobile Bug
+//
+//  Created by Brendon Duncan on 1/09/20.
+//  Copyright © 2020 Brendon Duncan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
